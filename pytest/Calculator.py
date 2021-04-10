03bf1e580534a07421a07dc4709153153d670142
shaozhenrui <823944257@qq.com>
2021-04-10 23:48:44 +0800

    pytest基础脚本

---
+++ pytest/Calculator.py
@@ -0,0 +1,15 @@
+"""
+__author__ = 'ceshiren_szr'
+__time__ = '2021/4/10 8:00'
+"""
+
+
+# 计算器
+class Calculator:
+    # 相加
+    def add(self, a, b):
+        return a + b
+
+    # 相除
+    def div(self, a, b):
+        return a / b
---
+++ pytest/test_algorithm.py
@@ -0,0 +1,34 @@
+"""
+__author__ = 'ceshiren_szr'
+__time__ = '2021/4/10 8:00'
+"""
+import pytest
+from Calculator import Calculator
+import yaml
+
+class TestCal:
+
+    def setup_class(self):
+        print("setup")
+        self.calc = Calculator()
+
+    def teardown_class(self):
+        print("teardown")
+
+    @pytest.mark.parametrize('a,b,expect', yaml.safe_load(open('./add.yaml')), ids=['int', 'float', 'zeronum'])
+    def test_add(self, a, b, expect):
+        assert expect == self.calc.add(a, b)
+
+    @pytest.mark.parametrize('a,b,expect', yaml.safe_load(open('./div.yaml')), ids=['int', 'zeronum', 'float'])
+    def test_div(self, a, b, expect):
+        if b != 0:
+            assert expect == self.calc.div(a, b)
+        else:
+            try:
+                self.calc.div(a, b)
+            except Exception as err:
+                assert expect in str(err)
+
+
+
+
